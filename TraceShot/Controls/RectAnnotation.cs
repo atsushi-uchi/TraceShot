3bd3afa559069d7c18f74a8ab4cb8d59f65d4b2e
atsushi <uchi.atsushi@gmail.com>
2026-03-15 19:06:17 +0900

    矩形の作成ができない不具合修正

--- TraceShot/Controls/RectAnnotation.cs
+++ TraceShot/Controls/RectAnnotation.cs
@@ -24,27 +24,27 @@ namespace TraceShot.Controls
         {
             Debug.WriteLine($"RectAnnotation OnUpdate {pos} {size}");
 
-            //base.OnUpdate(pos, size);
+            base.OnUpdate(pos, size);
 
-            //// 1. 左上座標を特定
-            //double left = Math.Min(_startPoint.X, pos.X);
-            //double top = Math.Min(_startPoint.Y, pos.Y);
+            // 1. 左上座標を特定
+            double left = Math.Min(_startPoint.X, pos.X);
+            double top = Math.Min(_startPoint.Y, pos.Y);
 
-            //// 2. 幅と高さを特定
-            //double w = Math.Abs(pos.X - _startPoint.X);
-            //double h = Math.Abs(pos.Y - _startPoint.Y);
+            // 2. 幅と高さを特定
+            double w = Math.Abs(pos.X - _startPoint.X);
+            double h = Math.Abs(pos.Y - _startPoint.Y);
 
-            //// 3. 絶対座標を更新
-            //X = left;
-            //Y = top;
-            //Width = w;
-            //Height = h;
+            // 3. 絶対座標を更新
+            X = left;
+            Y = top;
+            Width = w;
+            Height = h;
 
-            //// 4. 相対座標を更新
-            //RelX = Normalize(left, size.Width);
-            //RelY = Normalize(top, size.Height);
-            //RelWidth = Normalize(w, size.Width);
-            //RelHeight = Normalize(h, size.Height);
+            // 4. 相対座標を更新
+            RelX = Normalize(left, size.Width);
+            RelY = Normalize(top, size.Height);
+            RelWidth = Normalize(w, size.Width);
+            RelHeight = Normalize(h, size.Height);
         }
 
         public override bool OnComplete(ICollection<AnnotationBase> parentCollection)
